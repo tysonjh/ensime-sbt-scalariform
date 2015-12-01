8cb667194fefeac62cc4aa0c4f7c4653df62e547
Tyson Hamilton <tysonjh@google.com>
2015-12-01 12:09:26 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "test"
+
+scalaVersion := "2.10.4"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,7 @@
+object HelloWorld {
+  def main(args: Array[String]) {
+    var map: Map[String, Int] = Map()
+    map += ("blah" -> (1 + 1) % 1)
+    println("tada")
+  }
+}
